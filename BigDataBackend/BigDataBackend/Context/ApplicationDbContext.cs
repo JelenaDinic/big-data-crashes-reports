f5b4c26fef3a4c77218677acbe4cd8ac27f5555b
Jelena Dinic <jelenaad26@gmail.com>
2024-04-20 13:31:43 +0200

    upload project

---
+++ BigDataBackend/BigDataBackend/Context/ApplicationDbContext.cs
@@ -0,0 +1,31 @@
+﻿using BigDataBackend.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace BigDataBackend.Context
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext()
+        {
+
+        }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Crash> Crashes { get; set; } = null!;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Crash>()
+            .HasKey(cr => cr.CRASH_RECORD_ID);
+
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+        }
+    }
+
+}
---
+++ BigDataBackend/BigDataBackend/Model/Crash.cs
@@ -0,0 +1,24 @@
+﻿namespace BigDataBackend.Model
+{
+    public class Crash
+    {
+        public string CRASH_RECORD_ID { get; set; }
+        public string CRASH_DATE { get; set; } 
+        public string POSTED_SPEED_LIMIT { get; set; } 
+        public string TRAFFIC_CONTROL_DEVICE { get; set; }
+        public string WEATHER_CONDITION { get; set; }
+        public string FIRST_CRASH_TYPE { get; set; }
+        public string REPORT_TYPE { get; set; }
+        public string CRASH_TYPE { get; set; }
+        public string HIT_AND_RUN_I { get; set; }
+        public string STREET_DIRECTION { get; set; }
+        public string STREET_NAME { get; set; }
+        public string INJURIES_TOTAL { get; set; }
+        public string CRASH_HOUR { get; set; }
+        public string CRASH_DAY_OF_WEEK { get; set; }
+        public string CRASH_MONTH { get; set; }
+        public string LATITUDE { get; set; }
+        public string LONGITUDE { get; set; }
+        public string LANE_CNT { get; set; }
+    }
+}
